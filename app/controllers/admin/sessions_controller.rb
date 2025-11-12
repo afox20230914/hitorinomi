e8d0c8a8c10e383602ff278d546d4e0b3df73d4f
EC2 Default User <ec2-user@ip-172-31-22-11.ap-northeast-1.compute.internal>
2025-11-12 05:14:18 +0000

    Initial Commit

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 module Admin
   class SessionsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:create]
-    layout 'admin_login', only: [:new]  # ← これを追加！！
+    layout 'admin_login', only: [:new]
 
     def new
     end
 
     def create
-      admin = ::Administrator.find_by(email: params[:email])  # ← ← モジュール衝突を避ける
+      admin = ::Administrator.find_by(email: params[:email])
       if admin&.authenticate(params[:password])
         session[:admin_id] = admin.id
-        redirect_to admin_root_path, notice: "ログインしました" 
+        redirect_to admin_root_path, notice: "ログインしました"
       else
         flash.now[:alert] = 'メールアドレスまたはパスワードが違います。'
         render :new
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get    'login',  to: 'sessions#new'
     post   'login',  to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
+    get    'logout', to: 'sessions#destroy'
 
     resources :users,    only: [:index, :show, :destroy]
     resources :stores,   only: [:index, :show, :edit, :update, :destroy]

27d63af94690644d043c5a38cf1a5a1ef239f9b5
EC2 Default User <ec2-user@ip-172-31-22-11.ap-northeast-1.compute.internal>
2025-11-30 12:35:32 +0000

    migrate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
+    pp  session[:user_id]
+
     @current_user ||= User.find_by(id: session[:user_id])
+    pp @current_user
   end
 
   def logged_in?
